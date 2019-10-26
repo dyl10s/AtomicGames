6c0957a04632405d1d46260b86983e6bd41538e8
Dylan Strohschein <dylanstrohschein@gmail.com>
2019-10-26 11:31:46 -0400

    more work

--- ai/unitStrategies/WorkerStrategy.cs
+++ ai/unitStrategies/WorkerStrategy.cs
@@ -6,6 +6,8 @@ namespace ai.unitStrategies
 {
     public class WorkerStrategy
     {
+        public static Dictionary<int, string> navDirection = new Dictionary<int, string>();
+
         public static AICommand GetStrategy(IMap map, Unit unit)
         {
             var command = new AICommand();
@@ -24,10 +26,15 @@ namespace ai.unitStrategies
                     command.Command = AICommand.Gather;
                     command.Dir = AICommand.SerializeDirection(map.DirectionToAdjacentResource(unit.Location));
                 }
+
+                if(command.Dir == "no path")
+                {
+                    command.Dir = Explore(map, unit);
+                }
             }
             else
             {
-                command.Dir = AICommand.SerializeDirection(MapDirections.RandomDirection());
+                command.Dir = Explore(map, unit);
             }
 
             return command;
@@ -58,9 +65,13 @@ namespace ai.unitStrategies
                     }
                 }
 
-
                 var steps = finder.FindPath(unit.Location, closePoint, 1);
-                if(steps != null && steps.Count > 0)
+                if(steps == null)
+                {
+                    return "no path";
+                }
+
+                if(steps.Count > 0)
                 {
                     return Globals.directionToAdjactentPoint(unit.Location, steps[0]);
                 }
@@ -68,7 +79,6 @@ namespace ai.unitStrategies
                 {
                     return "None";
                 }
-                
             }
 
             return "None";
@@ -86,6 +96,80 @@ namespace ai.unitStrategies
             {
                 return "None";
             }
+        }
+
+        public static string Explore(IMap map, Unit unit)
+        {
+            PathFinder finder = new PathFinder(map);
+
+            if (!navDirection.ContainsKey(unit.Id))
+            {
+                navDirection.Add(unit.Id, AICommand.SerializeDirection(MapDirections.RandomDirection()));
+            }
+
+            var results = finder.FindPath(unit.Location, GetPointFromDir(unit.Location, navDirection[unit.Id]));
+
+            if(results == null)
+            {
+                var rnd = new Random();
+                if (navDirection[unit.Id] == "N" || navDirection[unit.Id] == "S")
+                {
+                    if(rnd.Next(0, 2) == 0)
+                    {
+                        navDirection[unit.Id] = "E";
+                    }
+                    else
+                    {
+                        navDirection[unit.Id] = "W";
+                    }
+                }
+                else if(navDirection[unit.Id] == "E" || navDirection[unit.Id] == "W")
+                {
+                    if (rnd.Next(0, 2) == 0)
+                    {
+                        navDirection[unit.Id] = "N";
+                    }
+                    else
+                    {
+                        navDirection[unit.Id] = "S";
+                    }
+                }
+            }
+            else
+            {
+                return Globals.directionToAdjactentPoint(unit.Location, results[0]);
+            }
+
+            return "None";
+        }
+
+        public static (int x, int y) GetPointFromDir((int x, int y)start, string dir)
+        {
+            if(dir == "N")
+            {
+                start.x -= 1;
+                return start;
+            }
+
+            if (dir == "S")
+            {
+                start.x += 1;
+                return start;
+            }
+
+            if (dir == "E")
+            {
+                start.y += 1;
+                return start;
+            }
+
+            if (dir == "W")
+            {
+                start.y -= 1;
+                return start;
+            }
+
+            return start;
         }
     }
 }
