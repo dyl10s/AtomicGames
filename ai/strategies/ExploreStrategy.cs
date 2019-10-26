5595e17a7eb251cbf707f2349a79130b1e83185c
Josef Hartsough <hartsouj@mail.gvsu.edu>
2019-10-26 10:58:52 -0400

    getting new version

--- ai/strategies/ExploreStrategy.cs
+++ ai/strategies/ExploreStrategy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ai.unitStrategies;
 
 namespace ai
 {
@@ -17,23 +18,24 @@ namespace ai
 
         public AICommand BuildCommand(Unit unit)
         {
+
+
             if (unit.IsScout)
             {
-
+                return ScoutStrategy.GetStrategy(Map, unit);
             }
 
             if (unit.IsWorker)
             {
-
+                return WorkerStrategy.GetStrategy(Map, unit);
             }
 
             if (unit.IsTank)
             {
-
+                return TankStrategy.GetStrategy(Map, unit);
             }
 
-            var direction = AICommand.SerializeDirection(MapDirections.RandomDirection());
-            return new AICommand { Command = AICommand.Move, Unit = unit.Id, Dir = direction };
+            return new AICommand {  };
         }
     }
 }
--- ai/unitStrategies/ScoutStrategy.cs
+++ ai/unitStrategies/ScoutStrategy.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace ai.unitStrategies
 {
-    class Scout
+    public class ScoutStrategy
     {
-        public AICommand GetStrategy(IMap map, Unit unit)
+        public static AICommand GetStrategy(IMap map, Unit unit)
         {
+            PathFinder pfinder = new PathFinder(map);
+
             var command = new AICommand();
 
 
+            command.Command = AICommand.Move;  
+            command.Unit = unit.Id;
+            command.Dir = Globals.North;
+
+            /*
+            if (hitsWall)
+            {
+                command.Dir = Globals.East;
+            }
+            //Still need to implement this bad boy
+            else if (enemyIsNear)
+            {
+                command.Dir = Globals.North;
+                command.Dir = Globals.South;
+
+            }
+
+            else if (map.EnemyBaseFound == true)
+            {
+                command.Dir = Globals.South;
+
+            }
+
+            else if(map.HasEnemies )
+
+            */
             return command;
         }
     }
